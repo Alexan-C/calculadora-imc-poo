3f290135872567915d82537fac0271d01f2a128c
Ale <alexandrecrz07@gmail.com>
2026-05-12 13:54:13 -0300

    Adição dos atletas e o Historico.

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,2 +1,28 @@
-public class Atleta {
+// extende de atleta podendo mudar
+public class Atleta extends Pessoa {
+  // isso que torna ele diferente
+    private String modalidade;
+
+
+    // diferencia dos demais incluindo modalidade
+    public Atleta(String nome, int idade , double peso, double altura, String modalidade) {
+        super(nome, idade, peso, altura);
+        this.modalidade = modalidade;
+    }
+    // calcula o IMC e volta para a interface pra poder ter um melhor controle
+    @Override
+    public String classificarIMC(double imc){
+        if (imc < 20.0 )return "Abaixo do peso ideal para o Atleta";
+        else if (imc < 27.0)  return "Peso ideal para o Atleta";
+        else  return "Acima do Peso ideal para o Atleta";
+
+    }
+    // retorna modadlidade
+    @Override
+    public String exibirPerfil(){
+        return super.exibirPerfil() + "| Modalidade: " + modalidade;
+    }
+
+    // retorna a modalidade para outros lugares do sistema caso precise
+    public String getModalidade() {return modalidade;}
 }
--- src/Historico.java
+++ src/Historico.java
@@ -1,2 +1,40 @@
+// importando Lista, pois no Java não se tem por nativo as listas
+import java.util.List;
+import java.util.ArrayList;
+
+// Pega a lista e coloca na memória, criando uma lista de armazenamento, podendo ser consultado se o programa nao for encerrado
 public class Historico {
-}
+
+    // registros das pessoas comuns
+    private List<String> registrosComum = new ArrayList<>();
+
+    // registros dos atletas
+    private List<String> registrosAtletas = new ArrayList<>();
+
+    // Se der erro
+    public void exibirRegistrosAtletas() {
+        if (registrosAtletas.isEmpty()) {
+            System.out.println("\n===Nenhum IMC encontrado ---");
+            return;
+        }
+        // imprime os nomes mais bonitos
+        System.out.println("\n=== HISTORICO DE IMC ===");
+        for (String registro : registrosAtletas) {
+            System.out.println(registro);
+        }
+    }
+
+    // se der erro
+    public void exibirRegistrosPessoas(){
+            if (registrosComum.isEmpty()){
+                System.out.println("\n===Nenhum IMC encontrado ---");
+                return;
+            }
+        }
+        // Imprime pessoas normais
+        System.out.println("\n=== HISTORICO DE IMC ===");
+        for (String registro : registrosComum) {
+            System.out.println(registro);
+        }
+    }
+
